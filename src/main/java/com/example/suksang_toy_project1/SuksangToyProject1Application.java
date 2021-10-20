4ee6612b462b6f10821bea3218da98c75d3a00f0
SangYu67 <murrmurr@naver.com>
2021-10-20 21:28:54 +0900

    211020 sang : start project

---
+++ src/main/java/com/example/suksang_toy_project1/SuksangToyProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.suksang_toy_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuksangToyProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuksangToyProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/suksang_toy_project1/SuksangToyProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suksang_toy_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuksangToyProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
